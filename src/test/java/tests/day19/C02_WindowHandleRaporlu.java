e0283d29df369890541622ba4124f9c9e115f2d1
Ferhan BARIN <ferhanbarin@yahoo.com>
2021-10-28 21:40:54 +0300

    POM

--- src/test/java/tests/day19/C02_WindowHandleRaporlu.java
+++ src/test/java/tests/day19/C02_WindowHandleRaporlu.java
@@ -12,13 +12,21 @@ public class C02_WindowHandleRaporlu extends TestBaseRapor {
 
     @Test
     public void test() {
+        extentTest = extentReports.createTest("Window Handle Test", "Yeni sayfada new window yazisini test eder.");
 
         Driver.getDriver().get("https://the-internet.herokuapp.com/windows");
+        extentTest.info("Istenen adrese gidildi");
 
         Driver.getDriver().findElement(By.linkText("Click Here")).click();
+        extentTest.info("Linke tiklandi.");
+
         ReusableMethods.switchToWindow("New Window");
+        extentTest.info("Yeni sayfaya gecildi.");
 
         WebElement newWindowTextElement = Driver.getDriver().findElement(By.tagName("h3"));
+        extentTest.info("Test edecegimiz yazi locate edildi.");
+
         Assert.assertTrue(newWindowTextElement.isDisplayed());
+        extentTest.info("Yazinin görünür oldugu test edildi.");
     }
 }
